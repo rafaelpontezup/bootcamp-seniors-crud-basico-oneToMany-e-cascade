f7558fa3e1ba3eeb99717359b848aef6e0adb792
Rafael Ponte <rafael.ponte@zup.com.br>
2022-02-16 17:40:49 -0300

    start LT2: imitation

--- src/main/java/br/com/zup/edu/financeiro/financeiro/notafiscal/NotaFiscalRespository.java
+++
@@ -1,8 +0,0 @@
-package br.com.zup.edu.financeiro.financeiro.notafiscal;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface NotaFiscalRespository extends JpaRepository<NotaFiscal, Long> {
-}
