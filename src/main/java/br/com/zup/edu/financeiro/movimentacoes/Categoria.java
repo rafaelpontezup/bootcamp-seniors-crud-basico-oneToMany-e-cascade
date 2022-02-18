45d162d204b09f635417b345663d19417afaa1ac
Rafael Ponte <rafael.ponte@zup.com.br>
2022-02-18 15:45:51 -0300

    gravaçao TC5.TN: API REST para deleçao

---
+++ src/main/java/br/com/zup/edu/financeiro/movimentacoes/Categoria.java
@@ -0,0 +1,31 @@
+package br.com.zup.edu.financeiro.movimentacoes;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Categoria {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Deprecated
+    public Categoria(){}
+
+    public Categoria(String nome) {
+        this.nome = nome;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+}
---
+++ src/main/java/br/com/zup/edu/financeiro/movimentacoes/CategoriaRepository.java
@@ -0,0 +1,8 @@
+package br.com.zup.edu.financeiro.movimentacoes;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+}
