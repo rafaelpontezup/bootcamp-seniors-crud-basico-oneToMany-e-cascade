aeef6d3815602540cda6428c379ef76902e7ac9d
Rafael Ponte <rafael.ponte@zup.com.br>
2022-03-30 16:22:43 -0300

    move para raiz para Spring scanear outros pacotes;

--- src/main/java/br/com/zup/edu/FinanceiroApplication.java
+++ src/main/java/br/com/zup/edu/FinanceiroApplication.java
@@ -1,4 +1,4 @@
-package br.com.zup.edu.financeiro;
+package br.com.zup.edu;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
